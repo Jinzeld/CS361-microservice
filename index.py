b859bf9deb100b1359a396157fcd0db099f97a49
Jinhui Zhen <123280886+Jinzeld@users.noreply.github.com>
2025-02-22 20:09:59 -0800

    updated files

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from bcrypt import hashpw, gensalt, checkpw
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from utils.db import add_user, find_user
 import os
 
@@ -21,6 +22,22 @@ def register():
     hashed_password = hashpw(password.encode('utf-8'), gensalt())
     add_user({"username": username, "password": hashed_password})
     return jsonify({"message": "User registered successfully"}), 201
+
+# User Login
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+
+    user = find_user(username)
+    if not user or not checkpw(password.encode('utf-8'), user['password']):
+        return jsonify({"message": "Invalid credentials"}), 401
+
+    token = create_access_token(identity=username)
+    return jsonify({"message": "Login successful", "token": token}) 
+
+
 
 if __name__ == 'main':
     app.run(debug=True)
